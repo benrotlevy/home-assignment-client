13dcbc1b5f37509e09672d741bd636bdffbdb83d
benrotlevy <benjamin@rotlevy.com>
2023-02-09 16:39:33 +0200

    responsive fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Error404 } from "./components/error404/Error404";
 import { HomePage } from "./components/homePage/homePage";
-import { SearchBar } from "./components/searchBar/SearchBar.js";
 import { AuthProvider } from "./context/context";
 import "bootstrap/dist/css/bootstrap.min.css";
 
